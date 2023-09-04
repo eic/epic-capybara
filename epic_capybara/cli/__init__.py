4a7a98238d0ae7099c8100a5abce08b92c0f137b
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-09-04 13:05:14 -0400

    capybara: disable invocation without subcommand

--- epic_capybara/cli/__init__.py
+++ epic_capybara/cli/__init__.py
@@ -7,7 +7,7 @@ from ..__about__ import __version__
 from .capy import capy
 from .bara import bara
 
-@click.group(context_settings={'help_option_names': ['-h', '--help']}, invoke_without_command=True)
+@click.group(context_settings={'help_option_names': ['-h', '--help']}, invoke_without_command=False)
 @click.version_option(version=__version__, prog_name='capybara')
 @click.pass_context
 def capybara(ctx: click.Context):
